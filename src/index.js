8f56a05b6b2cdd4f01cb70cd95202c767743ce0d
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2021-03-09 01:40:23 -0500

    Get React up and running.

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react"
+import ReactDOM from "react-dom"
+
+class Map extends Component {
+  render() {
+    return (
+      <div className="mapp">
+        <h1>Peace, dog</h1>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<Map />, document.getElementById("root"))
+    
