daf0414146e70b44fc226ff304a824dc7e181c07
Moacir P. de Sá Pereira <github@moacir.moacir.com>
2021-03-15 19:21:09 -0400

    Split patrons into only two groups and adjust colors.

--- map.js
+++ map.js
@@ -1,17 +1,16 @@
-const colors = [
-  "#1b9e77",
-  "#d95f02",
-  "#7570b3",
-  "#e7298a",
-  "#66a61e",
-  "#e6ab02",
-  "#a6761d",
-  "#666666",
-];
+const handlePopup = element => {
+  let popup = "<h2>";
+  if(element.url !== "") {
+    popup = popup + `<a href="${element.url}">${element.name}</a>`;
+  } else {
+    popup = popup + element.name;
+  }
+
+  return `${popup}</h2><p>${element.address}</p>`;
+};
 
 const addMap = async () => {
   data = await d3.csv("./data.csv");
-  layerValues = "america aristocrats jewish-16 nomads rich-people valery when-sylvia-met-james".split(" ");
 
   const map = L.map('map', {
     zoom: 16,
@@ -30,68 +29,46 @@ const addMap = async () => {
   });
   parisTiles.addTo(map);
 
-  const layers = layerValues.map( (group, i) =>
-    L.layerGroup(data
-      .filter(e => e.group === group)
-      .map(element =>
-        L.circleMarker([element.latitude, element.longitude], {
-          radius: 8,
-          fillColor: colors[i],
-          tier: element.rank,
-          color: "#fff",
-          weight: 3,
-          opacity: 1,
-          fillOpacity: 0.9 })
-          .bindPopup(`
-            <h2><a href="${element.url}">${element.name}</a></h2>
-            <p>${element.address}</p>
-          `)
-          .bindTooltip(L.tooltip({ 
-            opacity: 0.7, 
-            permanent: true, 
-            interactive: true })
-            .setContent(element.tooltipName)
-          )
-      )
+  const essaySubjects = L.layerGroup(data
+    .filter(e => e.group !== "others")
+    .map(element =>
+      L.circleMarker([element.latitude, element.longitude], {
+        radius: 10,
+        fillColor: "#7570b3",
+        tier: element.rank,
+        color: "#fff",
+        weight: 3,
+        opacity: 1,
+        fillOpacity: 0.9 })
+        .bindPopup(handlePopup(element))
+        .bindTooltip(L.tooltip({ 
+          opacity: 0.7, 
+          permanent: true, 
+          interactive: true })
+          .setContent(element.tooltipName)
+        )
     )
   );
 
-  layers.push(L.layerGroup(data
+  const otherPatrons = L.layerGroup(data
     .filter(e => e.group === "others")
     .map(element =>
       L.circleMarker([element.latitude, element.longitude], {
         radius: 5,
-        fillColor: colors[7],
+        fillColor: "#e6ab02",
         tier: element.rank,
-        color: "#fff",
-        weight: 1,
+        color: "#666",
+        weight: 1.5,
         opacity: 1,
         fillOpacity: 0.9 })
-        .bindPopup(`
-          <h2><a href="${element.url}">${element.name}</a></h2>
-          <p>${element.address}</p>
-        `)
+        .bindPopup(handlePopup(element))
       )
-    )
   );
 
-  const layerControl = {
-    "U.S. Institutions": layers[0],
-    "Aristocrats": layers[1],
-    "The Jewish 16th": layers[2],
-    "“Nomads”": layers[3],
-    "Rich People": layers[4],
-    "Valéry": layers[5],
-    "“When Sylvia Met James”": layers[6],
-    "Other Patrons": layers[7]
-  }
+  otherPatrons.addTo(map);
+  essaySubjects.addTo(map);
 
-  for (const layer of layers.reverse()) {
-    layer.addTo(map);
-  }
-
-
-  L.control.layers(null, layerControl).addTo(map);
+  const layers = [essaySubjects];
 
   const handleTooltips = () => {
     layers.forEach( layer => {
